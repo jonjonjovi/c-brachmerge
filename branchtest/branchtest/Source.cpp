060c343e470d6dc6a7fc9b4fc7fb106fa516188c
Hartnett, Jonathan <jonathan_hartnett@mcafee.com>
2019-02-06 12:02:37 +0000

    new line added

--- branchtest/branchtest/Source.cpp
+++ branchtest/branchtest/Source.cpp
@@ -6,6 +6,8 @@ int main()
     std::cout << num << std::endl;
     std::cout << func() << std::endl;
     std::cout << "adding line by reviewer" << std::endl;
+    std::cout << "adding line by coder" << std::endl;
+    std::cout << "adding line by coder2" << std::endl;
 }
 int func()
 {
